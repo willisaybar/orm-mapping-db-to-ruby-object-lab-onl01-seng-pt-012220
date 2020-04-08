59d3bd7f7e0b72de0f5db5317e43d2542bf81d1f
Willis Aybar <willisaybar@gmail.com>
2020-04-08 23:18:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(grade:)
+  def self.first_X_students_in_grade_10(grade)
     sql = <<-SQL
       SELECT *
       FROM students
