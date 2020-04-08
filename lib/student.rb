63be1fa392196366f762880c0d1bc8c985094987
Willis Aybar <willisaybar@gmail.com>
2020-04-08 23:21:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,13 +49,27 @@ class Student
       SELECT *
       FROM students
       WHERE students.grade = 10
-      LIMIT ? 
+      LIMIT ?
     SQL
 
     DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
     end
   end
+
+  def self..first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 
   def self.find_by_name(name)
     sql = <<-SQL
