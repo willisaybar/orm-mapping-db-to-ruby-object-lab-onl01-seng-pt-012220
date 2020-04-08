c4cb9b654f79ccaa3a701c5448700650d1b2d3ad
Willis Aybar <willisaybar@gmail.com>
2020-04-08 23:17:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,18 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10(grade:)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade = 10
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
